97096f60891392339538989d9eaaae6eaa68b10f
Liz Bayardelle <lizbayardelle@gmail.com>
2017-07-09 21:21:31 -0700

    First Commit in New Repo

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,3 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
-require 'bundler/setup' # OCRet up gems listed in the Gemfile.
+require 'bundler/setup' # LBDet up gems listed in the Gemfile.
